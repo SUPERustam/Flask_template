84e44cd099d6ad1c27b936d59bf6c6a722baa534
SUPERustam <498rustam@gmail.com>
2021-04-24 11:05:17 +0300

    minor update

--- data/news.py
+++ data/news.py
@@ -1,6 +1,7 @@
 import datetime
 import sqlalchemy
 from sqlalchemy import orm
+
 from .db_session import SqlAlchemyBase
 
 
--- forms/news.py
+++ forms/news.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from sqlalchemy import orm
 
 
 class NewsForm(FlaskForm):
